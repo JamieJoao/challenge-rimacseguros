ee27deba600071bed29a89d25739f953043bc531
Jamie Joao Mendoza Sernaqué <jojham1994@gmail.com>
2021-06-18 03:21:06 -0500

    [add] Soporte para SCSS

--- src/RimacApp.tsx
+++ src/RimacApp.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { MainRoute } from "./routes";
 import { AppState } from './components/layout';
 
+import './styles/global.scss'
+
 const RimacApp = () => {
   return (
     <AppState>
--- src/constants/routeNames.ts
+++ src/constants/routeNames.ts
@@ -1,3 +1,6 @@
+/**
+ * Indices para los nombres de las rutas
+ */
 export const routeNames = {
   home: '/',
   carData: '/datos-auto',
