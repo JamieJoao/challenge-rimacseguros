a94032da64d0d963eb8171b4ef73192dbcb261c2
Jamie Joao Mendoza Sernaqué <jojham1994@gmail.com>
2021-06-24 19:11:22 -0500

    [update] Actualización de HomeView

--- src/views/Home/HomeView.tsx
+++ src/views/Home/HomeView.tsx
@@ -1,11 +1,51 @@
-import { useBreakpoint } from 'hooks/useBreakpoint';
+import { useState } from 'react';
+import { useFormik } from 'formik'
+import validator from 'validator'
+
+import { Input, Button, Checkbox } from 'components/common';
+import { useFetch } from 'hooks/useFetch';
 import { Header, Container, Grid } from "components/layout";
+import { DNI_REGEX } from 'constants/index';
 
 import Image1Mobile from 'assets/images/img-1-mobile.svg'
 import styles from './Home.module.sass'
 
 export const HomeView = () => {
-  const { device } = useBreakpoint()
+  const { getUser, loading } = useFetch()
+  const [availableButton, setAvailableButton] = useState(false)
+  const [acceptTerms, setAcceptTerms] = useState(false)
+  const { values, handleChange, handleSubmit, errors } = useFormik({
+    initialValues: {
+      nrodoc: '78695362',
+      phone: '024-648-3804',
+      plate: '',
+    },
+    validate({ nrodoc, phone, plate }) {
+      const validatedErrors: any = {}
+
+      if (!DNI_REGEX.test(nrodoc)) {
+        validatedErrors.nrodoc = 'Documento inválido'
+      }
+      else if (validator.isEmpty(nrodoc)) {
+        validatedErrors.nrodoc = 'Requerido'
+      }
+
+      if (validator.isEmpty(phone)) {
+        validatedErrors.phone = 'Requerido'
+      }
+
+      if (validator.isEmpty(plate)) {
+        validatedErrors.plate = 'Requerido'
+      }
+
+      setAvailableButton(Object.keys(validatedErrors).length === 0)
+
+      return validatedErrors
+    },
+    onSubmit(data) {
+      getUser(data)
+    }
+  })
 
   return (
     <div className={styles.Home}>
@@ -36,12 +76,59 @@ export const HomeView = () => {
           <Container className={styles.form}>
             <span className={styles.text_title}>Déjanos tus datos</span>
 
-            <form>
-              
+            <form onSubmit={handleSubmit}>
+              <Input
+                id='nrodoc'
+                name='nrodoc'
+                label='Nro. de doc'
+                dropdown={[
+                  'DNI',
+                ]}
+                value={values.nrodoc}
+                onChange={handleChange}
+                hasError={errors.nrodoc}
+                disabled={loading}
+              />
+              <Input
+                id='phone'
+                name='phone'
+                label='Celular'
+                value={values.phone}
+                onChange={handleChange}
+                hasError={errors.phone}
+                disabled={loading}
+              />
+              <Input
+                id='plate'
+                name='plate'
+                label='Placa'
+                value={values.plate}
+                onChange={handleChange}
+                hasError={errors.plate}
+                disabled={loading}
+              />
+
+              <div className={styles.accept}>
+                <Checkbox
+                  accesoryRight={(
+                    <>
+                      <span>Acepto la </span>
+                      <a href='/#'>Política de Protección de Datos Personales</a>
+                      <span> y los </span>
+                      <a href='/#'>Términos y condiciones</a>
+                    </>
+                  )}
+                  onChange={setAcceptTerms} />
+              </div>
+
+              <Button title='cotízalo' type='submit' disabled={loading || !availableButton || !acceptTerms} />
             </form>
           </Container>
         </Grid>
       </Grid>
+
+      {/* <div>{JSON.stringify(values, null, 3)}</div> */}
+      {/* <div>{JSON.stringify(errors, null, 3)}</div> */}
     </div>
   )
 }
